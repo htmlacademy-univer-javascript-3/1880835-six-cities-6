2b0031db2c4beee2b6419c35babf525f57a537cd
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-09-22 14:40:58 +0300

    прописал маршруты к страницам

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Page404 } from './pages/404';
+import { Login } from './pages/Login';
 import { Main } from './pages/Main';
+import { Offer } from './pages/Offer';
 import { Place } from './place/types';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export interface AppProps {
   places: Place[];
 }
 export function App({ places }: AppProps) {
-  return <Main places={places} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Main places={places} />} />
+        <Route path="/offer/:id" element={<Offer />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
