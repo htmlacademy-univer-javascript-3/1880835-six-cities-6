6ee3a888b63d4828b9846dfb62f7784db7e9710d
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-09-22 14:41:21 +0300

    Переделал ссылку под useNavigate

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
 export function Page404() {
+  const navigate = useNavigate();
   return (
     <>
       <h1>404 Not Found</h1>
-      <a href="">На главную страницу</a>
+      <a
+        onClick={(event) => {
+          event.preventDefault();
+          navigate('/');
+        }}
+      >
+        На главную страницу
+      </a>
     </>
   );
 }
