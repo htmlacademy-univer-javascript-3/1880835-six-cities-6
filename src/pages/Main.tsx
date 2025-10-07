9360e28700a98ef5c33467b75dec8ed9da1a5896
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-10-07 22:37:27 +0300

    вынес список предложений в отдельный компонент

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,5 @@
 import { Place } from '../place/types';
-import { Card as PlaceCard } from '../place/components/Card';
+import { CardList } from '../place/components/CardList';
 
 export function Main({ places }: { places: Place[] }) {
   return (
@@ -83,42 +83,7 @@ export function Main({ places }: { places: Place[] }) {
         </div>
         <div className="cities">
           <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
-              <div className="cities__places-list places__list tabs__content">
-                {places.map((p, i) => (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <PlaceCard {...p} key={i} />
-                ))}
-              </div>
-            </section>
+            <CardList places={places} />
             <div className="cities__right-section">
               <section className="cities__map map"></section>
             </div>
