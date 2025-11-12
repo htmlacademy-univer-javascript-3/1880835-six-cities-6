6232bfbfe3df9bb3a1bc5b8861e15140b353f550
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-11-12 21:37:13 +0300

    исправил предупреждение о динамическом импорте

--- src/config/axios/index.ts
+++ src/config/axios/index.ts
@@ -1,19 +1,9 @@
 import axios from 'axios';
 import ENDPOINTS from './constants/ENDPOINTS.ts';
-import { selectAuthToken } from '../redux/slice/auth/selector';
 
 const api = axios.create({
   baseURL: 'https://14.design.htmlacademy.pro/six-cities',
   timeout: 5000,
-});
-
-api.interceptors.request.use(async (config) => {
-  const { store } = await import('../redux');
-  const token = selectAuthToken(store.getState());
-  if (token !== undefined) {
-    config.headers['X-Token'] = token;
-  }
-  return config;
 });
 
 export { ENDPOINTS, api };
